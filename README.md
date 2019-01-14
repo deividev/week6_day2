4aa4c3337cfc4982bbde8bd1b9a92783706ac165
Alex <alex@MacBook-Pro-de-Alex.local>
2019-01-14 13:15:29 +0100

    done w6d2

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
-# week6_day2
-Exercises of week 6 and day 2
+# Boream Campus BBDD
+
+Con este ejercicio vamos a practicar los callbacks y las llamadas Ajax con 
+la librería JQUERY. Hemos creado una API que contiene un listado de alumnos y profesores
+y debemos ser capaces de listar todos los recursos y crear nuevos.
+
+Recuerda que puedes ayudarte de la documentación oficial de Jquery si tienes
+alguna duda acerca de la librería.
+
+## Iteración 1
+En esta primera Iteración vamos a mostrar los alumnos y profesores almacenados en la base de datos.
+Lo primero que debemos hacer es registrar eventos onClick para los botones con los siguientes ids:
+
+Ambos eventos deben disparar la función searchResource.
+
+ ```
+ $("#btn-show-teachers").click(searchResource);
+ ```
+Debes completar la función searchResource para que realice la llamada a la función
+searchData pasandole como parámetro la variable endpoint. Dicha llamada debe implementar los callBacks 
+para cuando la llamada se realiza correctamente y cuando se produce algún error en la petición. En el caso de
+que la petición se realiza correctamente debemos llamar a la función handleData, por otro lado, si ocurre un error debemos
+llamar a handleError.
+
+Busca información acerca de los callbacks .done y .fail de Jquery.ajax.
+
+
+## Iteración 2
+Implementa la función searchData para que realice una petición Ajax al endpoint definido en la variable 
+ENPOINT_URL. La función debe retornar la respuesta de la petición y la petición debe hacerse mediante 
+la librería Jquery.
+
+Una vez realizado este punto, una vez se pulse en los botones "Mostrar Alumnos" o "Mostrar Profesores"
+ya deben pintarse los recursos traidos desde el servidor. 
+
+Como extra, gestiona la función handleError para que cree un elemento del DOM dentro del div
+fact que muestre un mensaje de error. 
+
+## Iteración 3
+En esta iteración vamos a realizar la funcionalidad de crear un nuevo alumno o un nuevo
+profesor en la BBDD. 
+
+Al igual que en la iteración 1, debes registrar eventos onClick para los botones con las siguientes ids:
+
+btn-create-teachers
+btn-create-students
+
+Ambos eventos deben disparar la función createForm. Que sera la encargada de crear los elementos DOM del formulario.
+
+Una vez el usuario completa la información del formulario y pulsa el botón de Enviar, se dispara la 
+función createResource. En dicha función debes realizar la llamada a createData, pasandole como parámetro la variable data, ademas, dicha llamada debe implementar los callbacks .done y .fail como en el punto anterior.
+
+
+## Iteración 4
+Implementa la función createData para que realice una llamada Ajax que cree un recurso nuevo en la base de datos.
+La llamada debe realizarse con Jquery y debe enviar los datos introducidos por el usuario en el formulario. 
+
+## Bonus 1
+En la función createElement, implementa la creación de un Boton que permita borrar el elemento en cuestión. 
+Dicho botón llamara a una nueva función deleteElement que realizara una petición DELETE para borrar el recurso de BBDD atacando al siguiente endpoint:
+
+/student/:id
+
+Por ejemplo, para borrar el estudiante con el id 2, debemos atacar al siguiente endpoint:
+
+http://5c3732177820ff0014d926b4.mockapi.io/student/2
+
+##Bonus 2
+
+Echa un vistazo al código base para ver el funcionamiento de las diferentes funciones que crean y eliminan 
+elementos del DOM. 
+
+Ya podemos listar y crear elementos nuevos, realiza los cambios necesarios en el código e implementa tus propias funciones
+para permitir a un usuario editar los datos de un profesor o alumno ya existente. 
+
+Recuerda que debes mostrar un formulario que permita ingresar los cambios y se envien al servidor mediante
+una nueva petición Patch.
+
+El endpoint al que se debe atacar es /student/:id. Por ejemplo, para editar el estudiante con id 2 la url completa es:
+
+http://5c3732177820ff0014d926b4.mockapi.io/student/2
---
+++ index.js
@@ -0,0 +1,117 @@
+let flagCreate = '';
+const ENDPOINT_URL = 'http://5c3732177820ff0014d926b4.mockapi.io/';
+$(function () {
+    $("#btn-create-student").click(createForm);
+    $("#btn-create-teacher").click(createForm);
+    $("#btn-show-teachers").click(searchResource);
+    $("#btn-show-students").click(searchResource);
+});
+
+function searchResource (e) {
+    let endpoint = e.target.innerHTML === 'Mostrar Alumnos' ? 'student' : 'teacher';
+    destroyElements();
+    $('#fact').addClass('fact');
+    // TODO Llamar a la función searchData y manejar las respuestas con los callbacks de los métodos .done y .fail de Jquery
+    // Todo .done() Debe llamar a handleData y .fail a handleError.
+}
+// Función que maneja la creación de nuevos recursos
+function createResource(e) {
+    // TODO llamar a la función getFormData, esta devolvera un objeto con los valores del formulario que hay que pasar a la llamada Ajax
+    // TODO Una vez obtenidos los datos del formulario, llamar a la función createData. .done debe llamar a destroyElements y .fail a handleError.
+    const data = getFormData(flagCreate);
+    createData(data).done(destroyElements).fail(handleError);
+}
+
+function createData(data) {
+    // Todo Realizar la llamada Ajax con Jquery para crear un recurso nuevo.
+    return $.ajax(ENDPOINT_URL+flagCreate, {
+        method: 'POST',
+        data: data,
+    })
+}
+function searchData (endpoint) {
+    //Todo Realizar la llamada Ajax con Jquery para traer los recursos.
+}
+// Almacena los valores del formulario en un objeto y lo retorna.
+function getFormData(endpoint) {
+    const obj = {};
+    if (endpoint === 'teacher') {
+        obj.email = $("#email").val();
+        obj.city = $("#city").val();
+    }
+    obj.name = $("#name").val();
+    return obj;
+}
+
+// Función que recibe un array de datos y crea un elemento en el DOM por cada uno de ellos.
+function handleData (data) {
+    if (data && data.length > 0) {
+        data.forEach((element) => {
+            createElement(element);
+        })
+    }
+}
+
+function handleError (err) {
+    // Manage Error
+}
+
+// Función que elimina todos los elementos existentes en el div fact.
+function destroyElements () {
+    $('#fact').empty();
+    $('#fact').removeClass('fact');
+}
+// Función que crea los elementos del DOM buscados.
+function createElement(data) {
+    const $icon = $("<img>", {id: "icon", "class": "icon-fact", src: data.avatar});
+    const $name = $("<p>", {id: 'name'});
+    const $id = $("<p>", {id: 'id'});
+    const $profileBox = $("<div>", {class: "profile-box"});
+    $id.append('Id: ' + data.id);
+    $name.append('Nombre: '+data.name);
+    $profileBox.append($icon);
+    $profileBox.append($name);
+    if (data.city || data.email) {
+        const $email = $("<p>", {id: 'email'});
+        const $city = $("<p>", {id: 'city'});
+        $email.append('Email: '+data.email);
+        $city.append('Ciudad: '+data.city);
+        $profileBox.append($email);
+        $profileBox.append($city);
+    }
+    $profileBox.append($id);
+    $('#fact').append($profileBox);
+}
+// Función que se encarga de crear el formulario con los campos necesarios para crear un nuevo recurso.
+function createForm(e) {
+    destroyElements();
+    $('#fact').addClass('fact');
+    let endpoint = e.target.innerHTML === 'Crear Alumno' ? 'student' : 'teacher';
+    flagCreate = endpoint;
+    const $title = $("<h3>", {});
+    const titleStr = e === 'teacher' ? 'Profesor' : 'Alumno';
+    $title.append('Crear '+titleStr);
+    const $inputName = $("<input>", {id: "name", "class": "input"});
+    const $labelName = $("<label>", {"class": "input-field"});
+    $labelName.append('Nombre: ');
+    const $submit = $("<button class='submit' />");
+    $submit.click(createResource);
+    $submit.append('Enviar');
+    $('#fact').append($title);
+    $('#fact').append($labelName)
+    $('#fact').append($inputName);
+    if (endpoint === 'teacher') {
+        const $labelEmail = $("<label>", {"class": "input-field"});
+        const $labelCity = $("<label>", {"class": "input-field"});
+        const $inputEmail = $("<input>", {id: "email", "class": "input"});
+        const $inputCity = $("<input>", {id: "city", "class": "input"});
+        $labelEmail.append('Email: ');
+        $labelCity.append('Ciudad: ');
+        $('#fact').append($labelEmail)
+        $('#fact').append($inputEmail);
+        $('#fact').append($labelCity)
+        $('#fact').append($inputCity);
+    }
+    $('#fact').append($submit);
+
+}
