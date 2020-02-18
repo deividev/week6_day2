91eb5dee67c35f17b421de840a810ecec0553ff7
Xamp <deivid.develop@gmail.com>
2020-02-18 19:34:45 +0100

    First commit, lack of iteration 4

--- alexClass.js
+++ alexClass.js
@@ -3,105 +3,83 @@ const ENDPOINT_URL = 'http://5c3732177820ff0014d926b4.mockapi.io/';
 $(function () {
     $("#btn-create-student").click(createForm);
     $("#btn-create-teacher").click(createForm);
-    $("#btn-show-teachers").click(searchResource);
-    $("#btn-show-students").click(searchResource);
+    $("#btn-show-teachers").click(() => {
+        searchResource('teacher')
+    });
+    $("#btn-show-students").click(() => {
+        searchResource('student')
+    });
 });
-
-function searchResource (event) {
-    let endpoint = event.target.innerHTML === 'Mostrar Alumnos' ? 'student' : 'teacher';
+function searchResource(partialEndpoint) {
+    // let endpoint = e.target.innerHTML === 'Mostrar Alumnos' ? 'student' : 'teacher';
     destroyElements();
     $('#fact').addClass('fact');
-    // TODO Llamar a la función searchData y manejar las respuestas con los callbacks de los métodos .done y .fail de Jquery
-    endpoint = ENDPOINT_URL + endpoint;
-    /*searchData(endpoint).done((respuesta) => {
-        console.log(respuesta);
-        handleData(respuesta);
-        debugger    
-    }) .fail((error) =>{
-        handleError(error);
-    })*/
+    let endpoint = ENDPOINT_URL + partialEndpoint;
     searchData(endpoint)
         .done(handleData)
         .fail(handleError);
-    
-    // Todo .done() Debe llamar a handleData y .fail a handleError.
 }
 // Función que maneja la creación de nuevos recursos
 function createResource(e) {
     // TODO llamar a la función getFormData, esta devolvera un objeto con los valores del formulario que hay que pasar a la llamada Ajax
-    // TODO Una vez obtenidos los datos del formulario, 
-    //llamar a la función createData. .done debe llamar a destroyElements y
-    // .fail a handleError.
+    // TODO Una vez obtenidos los datos del formulario, llamar a la función createData. .done debe llamar a destroyElements y .fail a handleError.
     const data = getFormData(flagCreate);
-    debugger    
-    createData(data)
-        .done(destroyElements)
-        .fail(handleError);
-    debugger
 }
-
-function createData(data, endpoint) {
+function createData(data) {
     // Todo Realizar la llamada Ajax con Jquery para crear un recurso nuevo.
-    $.get(endpoint, { userId : 35 }, function(resp) {
-        console.log(resp);
-    });
-    return $.ajax(data);
 }
-function searchData (endpoint) {
-   return $.ajax(endpoint);
-        
+function searchData(endpoint) {
     //Todo Realizar la llamada Ajax con Jquery para traer los recursos.
+    return $.ajax(endpoint);
 }
 // Almacena los valores del formulario en un objeto y lo retorna.
 function getFormData(endpoint) {
     const obj = {};
-    if (endpoint === 'teacher') {
+    if(endpoint === 'teacher') {
         obj.email = $("#email").val();
         obj.city = $("#city").val();
     }
     obj.name = $("#name").val();
     return obj;
 }
-
 // Función que recibe un array de datos y crea un elemento en el DOM por cada uno de ellos.
-function handleData (data) {
-    if (data && data.length > 0) {
+function handleData(data) {
+    debugger
+    if(data && data.length > 0) {
         data.forEach((element) => {
             createElement(element);
         })
     }
 }
-
-function handleError (err) {
-    // Manage Error
-    let misterios = $('#fact')
+function handleError(err) {
+    debugger
     if (err.status === 0) {
-        misterios.text('No hay conexion');
-    }else {
-        misterios.text(err.responseText);
+        $('#fact').text('No hay conexión')
+    } else {
+        $('#fact').text(err.responseText)
     }
+    // Manage Error
 }
-
 // Función que elimina todos los elementos existentes en el div fact.
-function destroyElements () {
+function destroyElements() {
     $('#fact').empty();
     $('#fact').removeClass('fact');
 }
 // Función que crea los elementos del DOM buscados.
 function createElement(data) {
-    const $icon = $("<img>", {id: "icon", "class": "icon-fact", src: data.avatar});
-    const $name = $("<p>", {id: 'name'});
-    const $id = $("<p>", {id: 'id'});
-    const $profileBox = $("<div>", {class: "profile-box"});
+    const $icon = $("<img>", { id: "icon", "class": "icon-fact", src: data.avatar });
+    const $name = $("<p>", { id: 'name' });
+    const $id = $("<p>", { id: 'id' });
+    const $profileBox = $("<div>", { class: "profile-box" });
     $id.append('Id: ' + data.id);
-    $name.append('Nombre: '+data.name);
+    $name.append('Nombre: ' + data.name);
     $profileBox.append($icon);
     $profileBox.append($name);
-    if (data.city || data.email) {
-        const $email = $("<p>", {id: 'email'});
-        const $city = $("<p>", {id: 'city'});
-        $email.append('Email: '+data.email);
-        $city.append('Ciudad: '+data.city);
+    if(data.city || data.email) {
+        const $email = $("<p>", { id: 'email' });
+        const $city = $("<p>", { id: 'city' });
+        $email.append('Email: ' + data.email);
+        $city.append('Ciudad: ' + data.city);
         $profileBox.append($email);
         $profileBox.append($city);
     }
@@ -116,9 +94,9 @@ function createForm(e) {
     flagCreate = endpoint;
     const $title = $("<h3>", {});
     const titleStr = e === 'teacher' ? 'Profesor' : 'Alumno';
-    $title.append('Crear '+titleStr);
-    const $inputName = $("<input>", {id: "name", "class": "input"});
-    const $labelName = $("<label>", {"class": "input-field"});
+    $title.append('Crear ' + titleStr);
+    const $inputName = $("<input>", { id: "name", "class": "input" });
+    const $labelName = $("<label>", { "class": "input-field" });
     $labelName.append('Nombre: ');
     const $submit = $("<button class='submit' />");
     $submit.click(createResource);
@@ -126,11 +104,11 @@ function createForm(e) {
     $('#fact').append($title);
     $('#fact').append($labelName)
     $('#fact').append($inputName);
-    if (endpoint === 'teacher') {
-        const $labelEmail = $("<label>", {"class": "input-field"});
-        const $labelCity = $("<label>", {"class": "input-field"});
-        const $inputEmail = $("<input>", {id: "email", "class": "input"});
-        const $inputCity = $("<input>", {id: "city", "class": "input"});
+    if(endpoint === 'teacher') {
+        const $labelEmail = $("<label>", { "class": "input-field" });
+        const $labelCity = $("<label>", { "class": "input-field" });
+        const $inputEmail = $("<input>", { id: "email", "class": "input" });
+        const $inputCity = $("<input>", { id: "city", "class": "input" });
         $labelEmail.append('Email: ');
         $labelCity.append('Ciudad: ');
         $('#fact').append($labelEmail)
